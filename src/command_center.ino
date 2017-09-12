01f9a4a3306ede32ae329f16bffcc77b6fc3b67a
Ubuntu <arunmozhi.rajavel@InfyLinuxVM.12fjojqfooeero5nk1vuoylxid.ix.internal.cloudapp.net>
2017-09-12 18:17:25 +0000

    commented in ino file

--- src/command_center.ino
+++ src/command_center.ino
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+//
 // This example only works with Arduino IDE 1.6.8 or later.
 
 #include <ESP8266WiFi.h>
