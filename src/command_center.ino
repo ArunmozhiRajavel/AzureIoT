31aacf5c62385e8e6e0efab9613b15a228648467
Arunmozhi Rajavel <arunmozhirajavel@gmail.com>
2017-10-23 15:13:32 +0530

    Update command_center.ino

--- src/command_center.ino
+++ src/command_center.ino
@@ -8,10 +8,23 @@
 #include "command_center.h"
 #include <AzureIoTHub.h>
 #include <AzureIoTProtocol_MQTT.h>
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
 
 const char ssid[] = "InfyIoT"; //  your WiFi SSID (name)
 const char pass[] = "20072117";    // your WiFi password (use for WPA, or use as key for WEP)
 const char connectionString[] = "HostName=InfyIoTHub.azure-devices.net;DeviceId=InfyTempCheck;SharedAccessKey=f1mLnJDGNL08+Wvn183ETJpu4o27ZyQkmvaEY+wEzDI=";
+const char* mqtt_server = "52.163.255.92";
+bool updateFirmware = 0;
+HTTPClient http;
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 int status = WL_IDLE_STATUS;
 
@@ -20,8 +33,60 @@ void setup() {
     initSerial();
     initWifi();
     initTime();
+    checkForUpdate();
+    reconnect();
+    callback(char* topic, byte* payload, unsigned int length);
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
 
 }
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+    updateFirmware = 1;
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+    updateFirmware = 0;
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
